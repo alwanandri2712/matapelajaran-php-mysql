8d08850a92402c8c936bf84b5fcd5a82c3b655bd
alwanandri2712 <47661419+alwanandri2712@users.noreply.github.com>
2019-10-31 14:56:34 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-$koneksi = mysqli_connect("localhost","u1168516_dinamis","dinamis123","u1168516_dinamis");
+$koneksi = mysqli_connect("localhost","root","","login");
 
 if (mysqli_connect_errno()){
 	echo "Koneksi database gagal : " . mysqli_connect_error();
